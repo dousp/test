ec8dde5400e3f30fa5b968a3bb02529b75e27700
douspeng <douspeng@sina.cn>
2019-09-04 20:39:03 +0800

    更新数组全部元素

--- src/main/java/com/dou/test/controller/MongoController.java
+++ src/main/java/com/dou/test/controller/MongoController.java
@@ -131,7 +131,6 @@ public class MongoController {
         Update update = new Update();
         // 不管是否存在都add一条
         // update.push("cart.items",item);
-
         // 将items变成了文档不再是数组
         // update.set("cart.items", item);
 
@@ -195,6 +194,21 @@ public class MongoController {
 
     }
 
+    /**
+     * 更新数组全部元素
+     * @param count
+     * @return
+     */
+    @PostMapping("/update/collection/{count}")
+    public UpdateResult updateCollection(@PathVariable Integer count){
+        Query query = Query.query(where("mobile").is("135")
+                .and("cart.type").is("超市")
+                .and("cart.items.price").gt(1)
+        );
+        Update update = new Update();
+        update.set("cart.items.$[].count", count);
 
+        return mongotemplate.upsert(query, update, Customer.class);
+    }
 
 }
