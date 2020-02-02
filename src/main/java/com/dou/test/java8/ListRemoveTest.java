bca95cc2c7dba91fd7a4d4a8809bf2d2de6960f0
Dousp <douspeng@sina.cn>
2020-02-02 12:53:00 +0800

    list

--- src/main/java/com/dou/test/java8/ListRemoveTest.java
+++ src/main/java/com/dou/test/java8/ListRemoveTest.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class listRemoveTest {
+public class ListRemoveTest {
 
     public static void main(String[] args) {
 
